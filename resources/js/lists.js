2043cbc8bc0585cfbbc86bf746d2eee5e461cc1a
Gustavo A <gustavo.atencio@outlook.com>
2022-07-20 16:47:22 -0500

    ref: click event into a function

--- resources/js/lists.js
+++ resources/js/lists.js
@@ -4,18 +4,24 @@
  */
 const $ = (selector) => document.querySelector(selector)
 
+/**
+ * 
+ * @param {MouseEvent} evt 
+ */
+function clickHandler(evt) {
+	/**
+ * @type {HTMLButtonElement}
+ */
+	const $target = evt.target
+	const $targetType = $target.getAttribute('type')
+	if ($targetType !== 'button') return
+	const $card = $target.closest('div.card')
+
+	console.log({ $card, productId: $card.dataset.productId })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	const $productContainer = $('#product-container');
 	if (!$productContainer) return
-	$productContainer.addEventListener("click", (evt) => {
-		/**
-		 * @type {HTMLButtonElement}
-		 */
-		const $target = evt.target
-		const $targetType = $target.getAttribute('type')
-		if ($targetType !== 'button') return
-		const $card = $target.closest('div.card')
-		
-		console.log({ $card, productId: $card.dataset.productId })
-	})
+	$productContainer.addEventListener("click", clickHandler)
 })
