90bfcfe8a3aabcee639af1e4b7b61f0cb3c33ec0
Gustavo A <gustavo.atencio@outlook.com>
2022-07-20 14:57:07 -0500

    db connection in mysql class

---
+++ app/Entities/MySQL.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entities;
+
+use Exception;
+use mysqli;
+
+require '../config/env.php';
+
+class MySQL
+{
+	private mysqli $connection;
+
+	public function connect($host, $username, $password, $database, $port)
+	{
+		$this->connection = new mysqli($host, $username, $password, $database, $port);
+		if ($this->connection->connect_errno) {
+			echo "Error while connect MySQL: ({$this->connection->connect_errno}) {$this->connection->connect_error}";
+		}
+	}
+
+	public function disconnect()
+	{
+		$this->connection = null;
+	}
+
+	public function prepare(string $query)
+	{
+		if ($this->connection == null) {
+			throw new Exception("Must be connected", 1);
+		}
+		return $this->connection->prepare($query);
+	}
+
+	public function query(string $query)
+	{
+		if ($this->connection == null) {
+			throw new Exception("Must be connected", 1);
+		}
+		return $this->connection->query($query);
+	}
+}
