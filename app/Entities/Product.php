676193e397ab04d014af30aa696fe28465b467b3
Gustavo A <gustavo.atencio@outlook.com>
2022-07-20 14:56:52 -0500

    id for products

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -4,14 +4,16 @@ namespace App\Entities;
 
 class Product
 {
+	public int $id;
 	public string $name;
 	public string $description;
 	public float $price;
 
-	public function __construct(string $name, string $description, float $price)
+	public function __construct(int $id, string $name, string $description, float $price)
 	{
-		$this->$name = $name;
-		$this->$description = $description;
-		$this->$price = $price;
+		$this->id = $id;
+		$this->name = $name;
+		$this->description = $description;
+		$this->price = $price;
 	}
 }
