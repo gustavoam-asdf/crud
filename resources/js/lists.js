e77557886a65a1e11da81a0febedf1c40d23c9c7
Gustavo A <gustavo.atencio@outlook.com>
2022-07-20 19:37:08 -0500

    load data on click button edit

--- resources/js/lists.js
+++ resources/js/lists.js
@@ -5,7 +5,23 @@
 const $ = (selector) => document.querySelector(selector)
 
 /**
- * 
+ * @param {HTMLDivElement} $card 
+ */
+function getProductCardContent($card) {
+	const $cardBody = $card.querySelector('div.card-body')
+
+	const product = {
+		id: Number($card.dataset.productId),
+		name: $cardBody.querySelector('h5.card-title').textContent.trim(),
+		description: $cardBody.querySelector('p.card-text').textContent.trim(),
+		price: Number($cardBody.querySelector('span.price').dataset.price.trim()),
+		imageUrl: $card.querySelector('img').src.trim()
+	}
+
+	return product
+}
+
+/**
  * @param {MouseEvent} evt 
  */
 function clickHandler(evt) {
@@ -15,9 +31,26 @@ function clickHandler(evt) {
 	const $target = evt.target
 	const $targetType = $target.getAttribute('type')
 	if ($targetType !== 'button') return
-	const $card = $target.closest('div.card')
 
-	console.log({ $card, productId: $card.dataset.productId })
+	const productInputs = {
+		$name: $('#product-name'),
+		$description: $('#product-description'),
+		$price: $('#product-price'),
+		$imageUrl: $('#product-url')
+	}
+	productInputs.$name.value = ""
+	productInputs.$description.value = ""
+	productInputs.$price.value = ""
+	productInputs.$imageUrl.value = ""
+
+	const $card = $target.closest('div.card')
+	const targetProduct = getProductCardContent($card)
+
+	productInputs.$name.value = targetProduct.name
+	productInputs.$description.value = targetProduct.description
+	productInputs.$price.value = targetProduct.price
+	productInputs.$imageUrl.value = targetProduct.imageUrl
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
